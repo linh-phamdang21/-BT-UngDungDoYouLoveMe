d68ff651cb6ba38ac5c61802f00039c7cfd2de51
linh-phamdang21 <ph.danglinh214@gmail.com>
2020-02-23 18:32:17 +0700

    Add File

---
+++ myjs.js
@@ -0,0 +1,4 @@
+function randomPosition() {
+    document.getElementById("btnno").style.top = Math.floor(Math.random() * 200) + "px";
+    document.getElementById("btnno").style.left = Math.floor(Math.random() * 400) + "px";
+}
